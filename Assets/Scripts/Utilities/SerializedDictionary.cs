c942b3badec3869112d0219409715fb414fb9e4d
shash-nerurkar <shash.nerurkar@gmail.com>
2026-03-20 04:58:10 +0530

    fixes serialized dictionary issues

--- Assets/Scripts/Utilities/SerializedDictionary.cs
+++ Assets/Scripts/Utilities/SerializedDictionary.cs
@@ -3,45 +3,59 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
-public class SerializedDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+namespace Scripts.Utilities
 {
+    /// <summary>
+    /// A serializable dictionary that can be used to store key-value pairs of any type.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
     [Serializable]
-    public struct Pair
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
     {
-        public TKey key;
-        public TValue value;
-
-        public static implicit operator KeyValuePair<TKey, TValue>(Pair pair)
+        [Serializable]
+        public struct Pair
         {
-            return new KeyValuePair<TKey, TValue>(pair.key, pair.value);
-        }
+            public TKey key;
+            public TValue value;
 
-        public static implicit operator Pair(KeyValuePair<TKey, TValue> pair)
-        {
-            return new Pair
+            public static implicit operator KeyValuePair<TKey, TValue>(Pair pair)
             {
-                key = pair.Key, 
-                value = pair.Value
-            };
+                return new KeyValuePair<TKey, TValue>(pair.key, pair.value);
+            }
+
+            public static implicit operator Pair(KeyValuePair<TKey, TValue> pair)
+            {
+                return new Pair
+                {
+                    key = pair.Key, 
+                    value = pair.Value
+                };
+            }
         }
-    }
 
-    [SerializeField] private List<Pair> entries = new();
+        [SerializeField] private List<Pair> entries = new();
 
-    public SerializedDictionary() {}
+        public SerializableDictionary() {}
 
-    public SerializedDictionary(IDictionary<TKey, TValue> dictionary) : base(dictionary) {}
+        public SerializableDictionary(IDictionary<TKey, TValue> dictionary) : base(dictionary) {}
 
-    public void OnBeforeSerialize() {
-        entries.Clear();
-        foreach (var pair in this)
-            entries.Add(pair);
-    }
+        public void OnBeforeSerialize()
+        {
+            
+        }
 
-    public void OnAfterDeserialize() {
-        Clear();
-        foreach (var entry in entries)
-            this[entry.key] = entry.value;
+        public void OnAfterDeserialize()
+        {
+            Clear();
+
+            foreach (var entry in entries)
+            {
+                if (entry.key == null || ContainsKey(entry.key))
+                    continue;
+
+                this[entry.key] = entry.value;
+            }
+        }
     }
 }
